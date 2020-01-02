ab0939c3ea5407d3e903e62632f017cac236fab5
Pascal Vautour <vautour.pascal@gmail.com>
2020-01-02 14:11:46 -0500

    Added basic tag crud

--- code/building-a-serverless-api-with-nodejs-and-aws-aurora-serverless/handler.js
+++ code/building-a-serverless-api-with-nodejs-and-aws-aurora-serverless/handler.js
@@ -1,111 +1,229 @@
-'use strict'
+"use strict";
 
-const connectToDatabase = require('./db')
-function HTTPError (statusCode, message) {
-  const error = new Error(message)
-  error.statusCode = statusCode
-  return error
+const connectToDatabase = require("./db");
+function HTTPError(statusCode, message) {
+  const error = new Error(message);
+  error.statusCode = statusCode;
+  return error;
 }
 
 module.exports.healthCheck = async () => {
-  await connectToDatabase()
-  console.log('Connection successful.')
+  await connectToDatabase();
+  console.log("Connection successful.");
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: 'Connection successful.' })
-  }
-}
+    body: JSON.stringify({ message: "Connection successful." })
+  };
+};
 
-module.exports.noteCreate = async (event) => {
+module.exports.noteCreate = async event => {
   try {
-    const { Note } = await connectToDatabase()
-    const note = await Note.create(JSON.parse(event.body))
+    const { Note } = await connectToDatabase();
+    const note = await Note.create(JSON.parse(event.body));
     return {
       statusCode: 200,
       body: JSON.stringify(note)
-    }
+    };
   } catch (err) {
-    console.log(err)
+    console.log(err);
     return {
       statusCode: err.statusCode || 500,
-      headers: { 'Content-Type': 'text/plain' },
-      body: 'Could not create the note. ' + err
-    }
+      headers: { "Content-Type": "text/plain" },
+      body: "Could not create the note. " + err
+    };
   }
-}
+};
 
-module.exports.noteGetOne = async (event) => {
+module.exports.noteGetOne = async event => {
   try {
-    const { Note } = await connectToDatabase()
-    const note = await Note.findById(event.pathParameters.id)
-    if (!note) throw new HTTPError(404, `Note with id: ${event.pathParameters.id} was not found`)
+    const { Note } = await connectToDatabase();
+    const note = await Note.findById(event.pathParameters.id);
+    if (!note)
+      throw new HTTPError(
+        404,
+        `Note with id: ${event.pathParameters.id} was not found`
+      );
     return {
       statusCode: 200,
       body: JSON.stringify(note)
-    }
+    };
   } catch (err) {
     return {
       statusCode: err.statusCode || 500,
-      headers: { 'Content-Type': 'text/plain' },
-      body: err.message || 'Could not fetch the Note.'
-    }
+      headers: { "Content-Type": "text/plain" },
+      body: err.message || "Could not fetch the Note."
+    };
   }
-}
+};
 
 module.exports.noteGetAll = async () => {
   try {
-    const { Note } = await connectToDatabase()
-    const notes = await Note.findAll()
+    const { Note } = await connectToDatabase();
+    const notes = await Note.findAll();
     return {
       statusCode: 200,
       body: JSON.stringify(notes)
-    }
+    };
   } catch (err) {
     return {
       statusCode: err.statusCode || 500,
-      headers: { 'Content-Type': 'text/plain' },
-      body: 'Could not fetch the notes.'
-    }
+      headers: { "Content-Type": "text/plain" },
+      body: "Could not fetch the notes."
+    };
   }
-}
+};
 
-module.exports.noteUpdate = async (event) => {
+module.exports.noteUpdate = async event => {
   try {
-    const input = JSON.parse(event.body)
-    const { Note } = await connectToDatabase()
-    const note = await Note.findById(event.pathParameters.id)
-    if (!note) throw new HTTPError(404, `Note with id: ${event.pathParameters.id} was not found`)
-    if (input.title) note.title = input.title
-    if (input.description) note.description = input.description
-    await note.save()
+    const input = JSON.parse(event.body);
+    const { Note } = await connectToDatabase();
+    const note = await Note.findById(event.pathParameters.id);
+    if (!note)
+      throw new HTTPError(
+        404,
+        `Note with id: ${event.pathParameters.id} was not found`
+      );
+    if (input.title) note.title = input.title;
+    if (input.description) note.description = input.description;
+    await note.save();
     return {
       statusCode: 200,
       body: JSON.stringify(note)
-    }
+    };
   } catch (err) {
     return {
       statusCode: err.statusCode || 500,
-      headers: { 'Content-Type': 'text/plain' },
-      body: err.message || 'Could not update the Note.'
-    }
+      headers: { "Content-Type": "text/plain" },
+      body: err.message || "Could not update the Note."
+    };
   }
-}
+};
 
-module.exports.noteDestroy = async (event) => {
+module.exports.noteDestroy = async event => {
   try {
-    const { Note } = await connectToDatabase()
-    const note = await Note.findById(event.pathParameters.id)
-    if (!note) throw new HTTPError(404, `Note with id: ${event.pathParameters.id} was not found`)
-    await note.destroy()
+    const { Note } = await connectToDatabase();
+    const note = await Note.findById(event.pathParameters.id);
+    if (!note)
+      throw new HTTPError(
+        404,
+        `Note with id: ${event.pathParameters.id} was not found`
+      );
+    await note.destroy();
     return {
       statusCode: 200,
       body: JSON.stringify(note)
-    }
+    };
   } catch (err) {
     return {
       statusCode: err.statusCode || 500,
-      headers: { 'Content-Type': 'text/plain' },
-      body: err.message || 'Could destroy fetch the Note.'
-    }
+      headers: { "Content-Type": "text/plain" },
+      body: err.message || "Could not destroy the Note."
+    };
   }
-}
+};
+
+module.exports.tagCreate = async event => {
+  try {
+    const { Tag } = await connectToDatabase();
+    const tag = await Tag.create(JSON.parse(event.body));
+    return {
+      statusCode: 200,
+      body: JSON.stringify(tag)
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      statusCode: err.statusCode || 500,
+      headers: { "Content-Type": "text/plain" },
+      body: "Could not create the tag. " + err
+    };
+  }
+};
+
+module.exports.tagGetOne = async event => {
+  try {
+    const { Tag } = await connectToDatabase();
+    const tag = await Tag.findById(event.pathParameters.id);
+    if (!tag)
+      throw new HTTPError(
+        404,
+        `Tag with id: ${event.pathParameters.id} was not found`
+      );
+    return {
+      statusCode: 200,
+      body: JSON.stringify(tag)
+    };
+  } catch (err) {
+    return {
+      statusCode: err.statusCode || 500,
+      headers: { "Content-Type": "text/plain" },
+      body: err.message || "Could not fetch the Tag."
+    };
+  }
+};
+
+module.exports.tagGetAll = async () => {
+  try {
+    const { Tag } = await connectToDatabase();
+    const tags = await Tag.findAll();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(tags)
+    };
+  } catch (err) {
+    return {
+      statusCode: err.statusCode || 500,
+      headers: { "Content-Type": "text/plain" },
+      body: "Could not fetch the tags."
+    };
+  }
+};
+
+module.exports.tagUpdate = async event => {
+  try {
+    const input = JSON.parse(event.body);
+    const { Tag } = await connectToDatabase();
+    const tag = await Tag.findById(event.pathParameters.id);
+    if (!tag)
+      throw new HTTPError(
+        404,
+        `Tag with id: ${event.pathParameters.id} was not found`
+      );
+    if (input.title) tag.title = input.title;
+    if (input.description) tag.description = input.description;
+    await tag.save();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(tag)
+    };
+  } catch (err) {
+    return {
+      statusCode: err.statusCode || 500,
+      headers: { "Content-Type": "text/plain" },
+      body: err.message || "Could not update the Tag."
+    };
+  }
+};
+
+module.exports.tagDestroy = async event => {
+  try {
+    const { Tag } = await connectToDatabase();
+    const tag = await Tag.findById(event.pathParameters.id);
+    if (!tag)
+      throw new HTTPError(
+        404,
+        `Tag with id: ${event.pathParameters.id} was not found`
+      );
+    await tag.destroy();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(tag)
+    };
+  } catch (err) {
+    return {
+      statusCode: err.statusCode || 500,
+      headers: { "Content-Type": "text/plain" },
+      body: err.message || "Could not destroy the Tag."
+    };
+  }
+};
