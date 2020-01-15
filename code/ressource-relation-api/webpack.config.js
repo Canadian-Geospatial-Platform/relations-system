dc776f103115b472a2b6a62b46bfbfd9b7737cdb
Pascal Vautour <vautour.pascal@gmail.com>
2020-01-15 13:11:36 -0500

    spring cleaning and added scripts

--- code/ressource-relation-api/webpack.config.js
+++ code/ressource-relation-api/webpack.config.js
@@ -5,12 +5,12 @@ const path = require("path");
 
 module.exports = {
   entry: slsw.lib.entries,
-  mode: "production",
+  mode: slsw.lib.webpack.isLocal ? "development" : "production",
   target: "node",
   externals: [nodeExternals(), "pg", "sqlite3", "tedious", "pg-hstore"],
   output: {
     libraryTarget: "commonjs",
-    path: path.resolve(__dirname + ".webpack"),
+    path: path.resolve(__dirname + ".webpack/" + Date.now() + ".webpack"), // Date.now allows to run multiple serverless-offline processes in parralel
     filename: "handler.js" // this should match the first part of function handler in serverless.yml
   },
   optimization: {
